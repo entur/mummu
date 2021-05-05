f8522e9d614369ddd8e96370259b46a173649a51
Tom Erik Støwer <testower@gmail.com>
2021-05-05 21:19:27 +0200

    More tests

--- src/test/java/org/entur/mummu/RestResourceIntegrationTest.java
+++ src/test/java/org/entur/mummu/RestResourceIntegrationTest.java
@@ -22,12 +22,72 @@ public class RestResourceIntegrationTest {
     private MockMvc mvc;
 
     @Test
+    public void testGetGroupOfStopPlacesById() throws Exception {
+        mvc.perform(get("/group-of-stop-places/NSR:GroupOfStopPlaces:1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name.value").value("Oslo"));
+    }
+
+    @Test
     public void testGetStopPlaceById() throws Exception {
-        mvc.perform(get("/stop-places/NSR:StopPlace:1")
+        mvc.perform(get("/stop-places/NSR:StopPlace:4004")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                    .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name.value").value("Jernbanetorget"));
+    }
+
+    @Test
+    public void testGetQuayById() throws Exception {
+        mvc.perform(get("/quays/NSR:Quay:7209")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.privateCode.value").value("11"));
+    }
+
+    @Test
+    public void testGetParkingById() throws Exception {
+        mvc.perform(get("/parkings/NSR:Parking:1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name.value").value("Drammen"));
+    }
+
+    @Test
+    public void testGetTopographicPlaceById() throws Exception {
+        mvc.perform(get("/topographic-places/KVE:TopographicPlace:0301")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content()
                 .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.name.value").value("Awesome stop"));
+                .andExpect(jsonPath("$.countryRef.ref").value("NO"));
+    }
+
+    @Test
+    public void testGetTariffZoneById() throws Exception {
+        mvc.perform(get("/tariff-zones/ATB:TariffZone:13")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name.value").value("E2"));
+    }
+
+    @Test
+    public void testGetFareZoneById() throws Exception {
+        mvc.perform(get("/fare-zones/BRA:FareZone:22")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name.value").value("Drammen"));
     }
 }
--- src/test/java/org/entur/mummu/TestNetexEntityIndexConfiguration.java
+++ src/test/java/org/entur/mummu/TestNetexEntityIndexConfiguration.java
@@ -1,21 +1,17 @@
 package org.entur.mummu;
 
-import org.entur.netex.index.NetexEntityIndex;
+import org.entur.netex.NetexParser;
 import org.entur.netex.index.api.NetexEntityIndexReadOnlyView;
-import org.rutebanken.netex.model.MultilingualString;
-import org.rutebanken.netex.model.StopPlace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
 
 @Configuration
 public class TestNetexEntityIndexConfiguration {
     @Bean
-    public NetexEntityIndexReadOnlyView netexEntityIndex() {
-        var index = new NetexEntityIndex();
-        var stopPlace = new StopPlace()
-                .withId("NSR:StopPlace:1")
-                .withName(new MultilingualString().withValue("Awesome stop"));
-        index.stopPlaceById.add(stopPlace);
-        return index.readOnlyView();
+    public NetexEntityIndexReadOnlyView netexEntityIndex() throws IOException {
+        var parser = new NetexParser();
+        return parser.parseFromZip("src/test/resources/IntegrationTestFixture.xml.zip");
     }
 }
