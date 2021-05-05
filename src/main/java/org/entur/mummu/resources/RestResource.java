a31cf4b58d114c5b981de078727bd058210e14cc
Tom Erik Støwer <testower@gmail.com>
2021-05-05 21:19:15 +0200

    Add endpoints for gosp and topigraphic-place

--- src/main/java/org/entur/mummu/resources/RestResource.java
+++ src/main/java/org/entur/mummu/resources/RestResource.java
@@ -2,10 +2,12 @@ package org.entur.mummu.resources;
 
 import org.entur.netex.index.api.NetexEntityIndexReadOnlyView;
 import org.rutebanken.netex.model.FareZone;
+import org.rutebanken.netex.model.GroupOfStopPlaces;
 import org.rutebanken.netex.model.Parking;
 import org.rutebanken.netex.model.Quay;
 import org.rutebanken.netex.model.StopPlace;
 import org.rutebanken.netex.model.TariffZone;
+import org.rutebanken.netex.model.TopographicPlace;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,8 +25,15 @@ public class RestResource {
         this.netexEntityIndex = netexEntityIndex;
     }
 
+    @GetMapping("/group-of-stop-places/{id}")
+    public GroupOfStopPlaces getGroupOfStopPlacesById(@PathVariable String id) {
+        return Optional.ofNullable(
+                netexEntityIndex.getGroupOfStopPlacesById().lookup(id)
+        ).orElseThrow(NotFoundException::new);
+    }
+
     @GetMapping("/stop-places/{id}")
-    public @ResponseBody StopPlace getStopPlaceById(@PathVariable String id) {
+    public StopPlace getStopPlaceById(@PathVariable String id) {
         return Optional.ofNullable(
                 netexEntityIndex.getStopPlaceById().lookup(id)
         ).orElseThrow(NotFoundException::new);
@@ -41,6 +50,13 @@ public class RestResource {
     public Parking getParkingById(@PathVariable String id) {
         return Optional.ofNullable(
                 netexEntityIndex.getParkingById().lookup(id)
+        ).orElseThrow(NotFoundException::new);
+    }
+
+    @GetMapping("/topographic-places/{id}")
+    public TopographicPlace getTopographicPlaceById(@PathVariable String id) {
+        return Optional.ofNullable(
+                netexEntityIndex.getTopographicPlaceById().lookup(id)
         ).orElseThrow(NotFoundException::new);
     }
 
