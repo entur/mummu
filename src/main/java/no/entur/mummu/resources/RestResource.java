594f4041b7e3da077e483a669878bf155196493f
Tom Erik Støwer <testower@gmail.com>
2021-05-06 22:22:18 +0200

    Add out-of-the-box swagger

--- src/main/java/no/entur/mummu/resources/RestResource.java
+++ src/main/java/no/entur/mummu/resources/RestResource.java
@@ -24,49 +24,49 @@ public class RestResource {
         this.netexEntityIndex = netexEntityIndex;
     }
 
-    @GetMapping("/group-of-stop-places/{id}")
+    @GetMapping(value = "/group-of-stop-places/{id}", produces = "application/json")
     public GroupOfStopPlaces getGroupOfStopPlacesById(@PathVariable String id) {
         return Optional.ofNullable(
                 netexEntityIndex.getGroupOfStopPlacesById().lookup(id)
         ).orElseThrow(NotFoundException::new);
     }
 
-    @GetMapping("/stop-places/{id}")
+    @GetMapping(value = "/stop-places/{id}", produces = "application/json")
     public StopPlace getStopPlaceById(@PathVariable String id) {
         return Optional.ofNullable(
                 netexEntityIndex.getStopPlaceById().lookup(id)
         ).orElseThrow(NotFoundException::new);
     }
 
-    @GetMapping("/quays/{id}")
+    @GetMapping(value = "/quays/{id}", produces = "application/json")
     public Quay getQuayById(@PathVariable String id) {
         return Optional.ofNullable(
                 netexEntityIndex.getQuayById().lookup(id)
         ).orElseThrow(NotFoundException::new);
     }
 
-    @GetMapping("/parkings/{id}")
+    @GetMapping(value = "/parkings/{id}", produces = "application/json")
     public Parking getParkingById(@PathVariable String id) {
         return Optional.ofNullable(
                 netexEntityIndex.getParkingById().lookup(id)
         ).orElseThrow(NotFoundException::new);
     }
 
-    @GetMapping("/topographic-places/{id}")
+    @GetMapping(value = "/topographic-places/{id}", produces = "application/json")
     public TopographicPlace getTopographicPlaceById(@PathVariable String id) {
         return Optional.ofNullable(
                 netexEntityIndex.getTopographicPlaceById().lookup(id)
         ).orElseThrow(NotFoundException::new);
     }
 
-    @GetMapping("/tariff-zones/{id}")
+    @GetMapping(value = "/tariff-zones/{id}", produces = "application/json")
     public TariffZone getTariffZoneById(@PathVariable String id) {
         return Optional.ofNullable(
                 netexEntityIndex.getTariffZonesById().lookup(id)
         ).orElseThrow(NotFoundException::new);
     }
 
-    @GetMapping("/fare-zones/{id}")
+    @GetMapping(value = "/fare-zones/{id}", produces = "application/json")
     public FareZone getFareZoneById(@PathVariable String id) {
         return Optional.ofNullable(
                 netexEntityIndex.getFareZoneById().lookup(id)
