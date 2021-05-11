ca59adaa40880450d7863ecfc5790b2b1df6bba2
Tom Erik Støwer <testower@gmail.com>
2021-05-11 15:34:38 +0200

    remove try/catch

--- src/main/java/no/entur/mummu/serializers/JAXBElementSerializer.java
+++ src/main/java/no/entur/mummu/serializers/JAXBElementSerializer.java
@@ -3,25 +3,18 @@ package no.entur.mummu.serializers;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.xml.bind.JAXBElement;
 import java.io.IOException;
 import java.io.Serializable;
 
 public class JAXBElementSerializer extends JsonSerializer<JAXBElement<?>> implements Serializable {
-    private final transient Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public void serialize(JAXBElement<?> jaxbElement, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-            try {
-                jsonGenerator.writeStartObject();
-                jsonGenerator.writeStringField("type", jaxbElement.getName().getLocalPart());
-                jsonGenerator.writeObjectField("value", jaxbElement.getValue());
-                jsonGenerator.writeEndObject();
-            } catch (IOException e) {
-                logger.warn("Caught exception while serializing JAXBElement={}", jaxbElement, e);
-            }
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeStringField("type", jaxbElement.getName().getLocalPart());
+        jsonGenerator.writeObjectField("value", jaxbElement.getValue());
+        jsonGenerator.writeEndObject();
     }
 }
