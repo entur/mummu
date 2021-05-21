397a0d2150d03c416d1ac31fe06f6fa2003b6fd1
Tom Erik Støwer <testower@gmail.com>
2021-05-21 14:17:41 +0200

    Set swagger server urls manually

---
+++ src/main/java/no/entur/mummu/config/SwaggerConfiguration.java
@@ -0,0 +1,23 @@
+package no.entur.mummu.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Value("${no.entur.mummu.swagger.host.url}")
+    private String hostUrl;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl(hostUrl);
+        return new OpenAPI().servers(List.of(server));
+    }
+}
--- src/main/java/no/entur/mummu/resources/StopPlacesResource.java
+++
@@ -1,2 +0,0 @@
-package no.entur.mummu.resources;public class StopPlacesResource {
-}
