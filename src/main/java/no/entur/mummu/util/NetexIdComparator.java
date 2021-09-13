8550bd0f482ac39219ae88874100a56f01ab3bbe
Tom Erik Støwer <testower@gmail.com>
2021-09-13 14:10:54 +0200

    Fix id comparator

--- src/main/java/no/entur/mummu/util/NetexIdComparator.java
+++ src/main/java/no/entur/mummu/util/NetexIdComparator.java
@@ -7,9 +7,15 @@ import java.util.Comparator;
 public class NetexIdComparator implements Comparator<EntityStructure> {
     @Override
     public int compare(EntityStructure o1, EntityStructure o2) {
-        return Integer.compare(
-                Integer.parseInt(o1.getId().split(":")[2]),
-                Integer.parseInt(o2.getId().split(":")[2])
-        );
+        String firstId = o1.getId();
+        String secondId = o2.getId();
+
+        int sorted = Integer.compare(firstId.length(), secondId.length());
+
+        if (sorted == 0) {
+            return firstId.compareTo(secondId);
+        } else {
+            return sorted;
+        }
     }
 }
