84dc9f4845e7b05799f7f533fb652c22c0d082b6
Tom Erik Støwer <testower@gmail.com>
2021-11-04 12:13:54 +0100

    Log time taken to parse netex file

--- src/main/java/no/entur/mummu/config/NetexEntityIndexConfiguration.java
+++ src/main/java/no/entur/mummu/config/NetexEntityIndexConfiguration.java
@@ -2,6 +2,8 @@ package no.entur.mummu.config;
 
 import org.entur.netex.NetexParser;
 import org.entur.netex.index.api.NetexEntitiesIndex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -12,11 +14,14 @@ import java.io.IOException;
 @Configuration
 public class NetexEntityIndexConfiguration {
     private final NetexEntitiesIndex netexEntitiesIndex;
+    private static final Logger log = LoggerFactory.getLogger(NetexEntityIndexConfiguration.class);
 
     @Autowired
     public NetexEntityIndexConfiguration(@Value("${no.entur.mummu.data-file}") String dataFile) throws IOException {
         var parser = new NetexParser();
+        long now = System.currentTimeMillis();
         netexEntitiesIndex = parser.parse(dataFile);
+        log.info("Parsed NeTEx file in {} ms", System.currentTimeMillis() - now);
     }
 
     @Bean
