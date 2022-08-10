e30fd99c8a12c5afbc48837d2b755bea340faf04
Tom Erik Støwer <testower@gmail.com>
2022-08-10 13:55:45 +0200

    Add rate limiting

---
+++ api/mummu/apiproxy/resources/jsc/javaScript.rateLimitValues.js
@@ -0,0 +1,28 @@
+
+var etClientName = context.getVariable("request.header.et-client-name");
+
+var identifiedUser = {
+    quota: "2000", spikeArrest: "150ps"
+};
+
+var unidentifiedUser = {
+    quota: "30", spikeArrest: "2ps"
+};
+
+if (etClientName) { // if client identified
+    quotaAllowed = identifiedUser.quota;
+    spikeArrestAllowed = identifiedUser.spikeArrest;
+} else { // if not identified -> rate on ip
+    etClientName = "unkown-" + context.getVariable("client.ip")
+    quotaAllowed = unidentifiedUser.quota;
+    spikeArrestAllowed = unidentifiedUser.spikeArrest;
+    // Add et-client-name with ip
+    context.setVariable("request.header.Et-Client-Name", etClientName);
+}
+
+context.setVariable("quota.client.identifier", etClientName);
+context.setVariable("spikeArrest.client.identifier", etClientName);
+
+// et quota and spike limits
+context.setVariable("quota.client.allowed", quotaAllowed);
+context.setVariable("spikeArrest.client.allowed", spikeArrestAllowed);
