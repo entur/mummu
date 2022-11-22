cc65514028368f8e5500efa21584507943612b28
Assad Riaz <assad.riaz@entur.org>
2022-11-22 15:54:00 +0100

    Updates docker entrypoint, netex file download location.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,6 @@ echo "entrypoint"
 : ${NETEX_DATA_URL="https://storage.googleapis.com/marduk-production/tiamat/CurrentwithServiceFrame_latest.zip"}
 
 echo "Downloading $NETEX_DATA_URL"
-wget -O netex_data.zip $NETEX_DATA_URL
+wget -O /tmp/netex_data.zip $NETEX_DATA_URL
 
 exec "$@"
