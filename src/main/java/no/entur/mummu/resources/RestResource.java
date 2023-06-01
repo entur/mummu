048217cbc2f5a6b391e9c88ea61fcd97a64df390
Tom Erik Støwer <testower@gmail.com>
2023-06-01 12:37:24 +0200

    Revert "Attempt to fix serving static resources"
    This reverts commit 0accb6971a1dcb145a17faf494c9690cc5631ddc.

--- src/main/java/no/entur/mummu/resources/RestResource.java
+++ src/main/java/no/entur/mummu/resources/RestResource.java
@@ -22,7 +22,6 @@ import org.rutebanken.netex.model.VehicleModeEnumeration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +31,6 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/")
 public class RestResource {
     private final NetexEntitiesService netexEntitiesService;
     private static final NetexObjectFactory netexObjectFactory = new NetexObjectFactory();
