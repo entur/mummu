2e5849f81411f58bcb925687f46a6abea18733db
Tom Erik Støwer <testower@gmail.com>
2023-09-27 21:13:50 +0200

    Add error controller

---
+++ src/main/java/no/entur/mummu/resources/RestErrorController.java
@@ -0,0 +1,41 @@
+package no.entur.mummu.resources;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+public class RestErrorController implements ErrorController {
+    private static final String ERROR_PATH = "/error";
+
+    /**
+     * Supports other formats like JSON, XML
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = ERROR_PATH)
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        HttpStatus status = getStatus(request);
+        return new ResponseEntity<>(status);
+    }
+
+    private HttpStatus getStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request
+                .getAttribute("javax.servlet.error.status_code");
+        if (statusCode != null) {
+            try {
+                return HttpStatus.valueOf(statusCode);
+            }
+            catch (Exception ignored) {
+            }
+        }
+        return HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+}
