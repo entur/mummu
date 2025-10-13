5f76cfedc91148b136e61bb897d7c71e932c3966
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-10-13 12:02:06 +0200

    Remove @Parameter annotations as it interfered with @ParameterObject and created awkward dependency from service object

--- src/main/java/no/entur/mummu/services/StopPlacesRequestParams.java
+++ src/main/java/no/entur/mummu/services/StopPlacesRequestParams.java
@@ -1,6 +1,5 @@
 package no.entur.mummu.services;
 
-import io.swagger.v3.oas.annotations.Parameter;
 import no.entur.mummu.util.MultimodalFilter;
 import org.rutebanken.netex.model.StopTypeEnumeration;
 import org.rutebanken.netex.model.VehicleModeEnumeration;
@@ -8,28 +7,13 @@ import org.rutebanken.netex.model.VehicleModeEnumeration;
 import java.util.List;
 
 public record StopPlacesRequestParams(
-        @Parameter(description = "Maximum number of results to return", example = "10")
         Integer count,
-
-        @Parameter(description = "Number of results to skip", example = "0")
         Integer skip,
-
-        @Parameter(description = "Filter by specific IDs")
         List<String> ids,
-
-        @Parameter(description = "Filter by multimodal type", example = "both")
         MultimodalFilter.MultimodalFilterType multimodal,
-
-        @Parameter(description = "Filter by transport modes")
         List<VehicleModeEnumeration> transportModes,
-
-        @Parameter(description = "Filter by stop place types")
         List<StopTypeEnumeration> stopPlaceTypes,
-
-        @Parameter(description = "Filter by topographic place IDs")
         List<String> topographicPlaceIds,
-
-        @Parameter(description = "Filter by quay IDs")
         List<String> quayIds
 ) {
     public StopPlacesRequestParams {
